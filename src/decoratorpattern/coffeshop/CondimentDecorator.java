03c3263a8b85270c3ea68be10051023786d28613
SijiChen <chensiji19891@gmail.com>
2017-01-25 14:54:51 -0500

    decorator pattern

---
+++ src/decoratorpattern/coffeshop/CondimentDecorator.java
@@ -0,0 +1,5 @@
+package decoratorpattern.coffeshop;
+
+public abstract class CondimentDecorator extends Beverage {
+	public abstract String getDescription();
+}
---
+++ src/decoratorpattern/coffeshop/Milk.java
@@ -0,0 +1,17 @@
+package decoratorpattern.coffeshop;
+
+public class Milk extends CondimentDecorator {
+	Beverage beverage;
+
+	public Milk(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	public String getDescription() {
+		return beverage.getDescription() + ", Milk";
+	}
+
+	public double cost() {
+		return .10 + beverage.cost();
+	}
+}
