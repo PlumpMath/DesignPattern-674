fb21c3b0d93e5f16b9cba649474f371a1b68fce9
SijiChen <chensiji19891@gmail.com>
2017-01-26 09:16:03 -0500

    add singleton

---
+++ src/singletonpattern/classic/SingletonClient.java
@@ -0,0 +1,8 @@
+package singletonpattern.classic;
+
+public class SingletonClient {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton.getDescription());
+	}
+}
